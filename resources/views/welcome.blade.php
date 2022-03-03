306d2dcb20f5f1c4ba2d8d89a5943290b5d1881d
elcon14 <amadorelias@hotmail.com>
2022-03-02 18:07:14 -0600

    prueba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,4 +5,7 @@
 <link href="{{asset ('plugins/bootstrap-datepicker/dist/css/bootstrap-datepicker.css')}}" rel="stylesheet" />
 @endsection
 
+@section('container')
+hola mundo
+@endsection
 @section('title', 'Welcome')
